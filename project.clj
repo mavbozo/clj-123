a7a6e39ba30559cdfd5615623928f3200d4b3889
Avicenna <maverickbozo@me.com>
2015-09-28 20:06:17 +0700

    update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :codox {:output-dir "../clj-123-doc"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [commons-codec "1.5"]
-                 [clj-time "0.6.0"]
-                 [org.clojure/data.xml "0.0.7"]]
-   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
-                                  [clj-http "0.9.2"]]
+  :dependencies [[org.clojure/clojure "1.8.0-alpha5"]
+                 [commons-codec "1.10"]
+                 [clj-time "0.11.0"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [clj-http "2.0.0"]
+                 [org.clojure/test.check "0.8.2"]
+                 [prismatic/schema "1.0.1"]]
+   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :source-paths ["dev"]}}
    :repl-options {:init-ns user})
 
